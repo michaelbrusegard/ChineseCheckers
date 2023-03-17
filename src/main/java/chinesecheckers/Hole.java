d0097abcec5dc402b13ec87cd8ba0607390dd73d
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2023-03-17 21:36:38 +0100

    Made the board in App.fxml, started Hole.java

---
+++ src/main/java/chinesecheckers/Hole.java
@@ -0,0 +1,14 @@
+package chinesecheckers;
+
+public class Hole {
+    
+    boolean occupied = false;
+
+    public void setOccupied(boolean occupied) {
+        this.occupied = occupied;
+    }
+
+    public boolean getOccupied() {
+        return this.occupied;
+    }
+}
